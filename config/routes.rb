e528407ad485b6df09da521a8db6214d53f197c8
kip_parcell <kipluis@gmail.com>
2021-02-27 19:35:25 -0700

    commit tree 7e2955e9440bdd22215b7ef472b481b4581f0888

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
+  get 'genres/create'
+
+  get 'genres/update'
+
+  get 'genres/show'
+
+  get 'artists/create'
+
+  get 'artists/update'
+
+  get 'artists/show'
+
+  get 'songs/create'
+
+  get 'songs/update'
+
+  get 'songs/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :songs
+  resources :genres
+  resources :artists
 end
